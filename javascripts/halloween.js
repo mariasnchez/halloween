dfc37109f5c5572565c2e3cf9ad8facc4f17574d
mariasnchez <91953276+mariasnchez@users.noreply.github.com>
2021-11-16 21:59:16 +0100

    Add files via upload

---
+++ javascripts/halloween.js
@@ -0,0 +1,13 @@
+/*functions to use inside halloween exercise*/
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible"
+}
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden"
+}
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+function parar(sonido){
+    document.getElementById(sonido).pause()
+}
